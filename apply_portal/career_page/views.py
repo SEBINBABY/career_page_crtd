32f3f7eeafbd5700babb386569aca32e07850e7f
SEBIN BABY <sebin.18@gmail.com>
2025-01-10 00:06:00 +0530

    views.py

--- apply_portal/career_page/views.py
+++ apply_portal/career_page/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+def education_info(request):
+    return render(request, "Education_information.html")
+
+def education_info(request):
+    return render(request, "Education_information.html")
+
+def education_info(request):
+    return render(request, "Education_information.html")
+
+def education_info(request):
+    return render(request, "Education_information.html")
+
+def education_info(request):
+    return render(request, "Education_information.html")
+
+def education_info(request):
+    return render(request, "Education_information.html")
+
+def education_info(request):
+    return render(request, "Education_information.html")
